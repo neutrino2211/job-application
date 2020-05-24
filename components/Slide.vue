1a1b326bcb7ad5a066b35544b8d624b54a0647f7
neutrino2211 <neutrino2211@gmail.com>
2020-05-24 18:01:27 +0100

    [Slides]: Improved slide movement UX and added slides component

---
+++ components/Slide.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :slide-id="slideId" class="slide text-center min-h-screen py-32 mx-auto md:w-5/6 xl:w-2/3">
+        <h1 style="font-size: 3rem;"> <slot></slot> </h1>
+
+        <div>
+            <h2 class="mx-auto py-8">
+                <slot name="description"></slot>
+            </h2>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        slideId: Number,
+    }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <input ref="checkbox" type="checkbox" name="creative_mode" id="creative">
       <div class="toggle"></div>
     </div>
-    <div slide-id="0" class="slide text-center h-screen py-32 mx-auto md:w-5/6 xl:w-2/3">
+    <div slide-id="0" class="slide text-center min-h-screen py-32 mx-auto md:w-5/6 xl:w-2/3">
       <h1 style="font-size: 3rem;"> <span style="font-size: 4rem;">Hello! 👋</span> <br> <br> My Name is Tsowa Mainasara Al-amin and this is why i'll be a great fit. </h1>
 
       <div>
@@ -14,7 +14,7 @@
       </div>
     </div>
 
-    <div slide-id="1" class="slide text-center h-screen pb-32 mx-auto md:w-5/6 xl:w-2/3">
+    <div slide-id="1" class="slide text-center min-h-screen pb-32 mx-auto md:w-5/6 xl:w-2/3">
       <h1 style="font-size: 3rem;"> <br> <br> That's all I have to say. Thanks for viewing my application! 🎉🎉</h1>
 
       <div>
@@ -27,11 +27,19 @@
       </div>
     </div>
 
+    <slide slideId="2">
+       <span><span style="font-size: 4rem;">Hello! 👋</span> <br> <br> My Name is Tsowa Mainasara Al-amin and this is why i'll be a great fit. </span>
+
+       <template v-slot:description>
+         <span>A description</span>
+       </template>
+    </slide>
+
     <div class="mx-auto w-full py-4 arrow-container">
-      <div role="button" v-if="slideNumber + 1 < slideCount" class="arrow" @click="moveSlide(1)">
+      <div role="button" v-bind:class="{ show: slideNumber + 1 < slideCount }" class="arrow" @click="moveSlide(1)">
         <img class="mx-auto my-auto h-6" src="~/assets/arrow.svg" alt="" srcset="">
       </div>
-      <div role="button" v-if="slideNumber > 0" class="arrow" @click="moveSlide(-1)">
+      <div role="button" v-bind:class="{ show: slideNumber > 0 }" class="arrow" @click="moveSlide(-1)">
         <img class="mx-auto my-auto h-6" src="~/assets/arrow-up.svg" alt="" srcset="">
       </div>
     </div>
@@ -40,11 +48,11 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Logo from "~/components/Logo.vue";
+import Slide from "~/components/Slide.vue";
 
 export default Vue.extend({
   components: {
-    Logo
+    Slide
   },
 
   data: ()=>({
@@ -83,13 +91,17 @@ export default Vue.extend({
 
           callback(Number(el.getAttribute('slide-id') || 0));
         }
-      }, { threshold: [1] });
+      }, { threshold: [0.5] });
 
       observer.observe(element);
     },
 
     moveSlide(i: number) {
-      this.slideNumber = this.slideNumber + i;
+      const newNumber = this.slideNumber + i;
+
+      if (newNumber < 0 || newNumber >= this.slideCount) return;
+
+      this.slideNumber = newNumber;
       const query = `div[slide-id="${this.slideNumber}"]`;
       const slide = document.querySelector(query);
       slide?.scrollIntoView({
@@ -108,10 +120,11 @@ p {
 }
 
 .arrow {
-  transition: transform 0.2s ease-in;
+  transition: all 0.2s ease-in;
   padding: 5px;
   color: #afe9c6;
   display: inline-flex;
+  opacity: 0.3;
 
   @apply w-10 mx-2;
 }
@@ -128,10 +141,13 @@ img {
   cursor: pointer;
 }
 
-.arrow:hover {
-  transform: scale(1.1);
+.arrow:not(.show):hover {
+  transform: initial;
 }
 
+.arrow.show:hover {
+  transform: scale(1.1);
+}
 
 .toggle-background {
   background-color: #7bac8e;
@@ -161,5 +177,9 @@ img {
     transition: 0.4s;
     @apply w-4 h-4;
   }
+}
+
+.show {
+  opacity: 1;
 }
 </style>
